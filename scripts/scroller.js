5de56f21fa966dc17151435d4597e9edb85e98f4
Aedan Cullen <asc.cn04@gmail.com>
2015-10-15 13:08:32 -0400

    Make smooth-scrolling quicker

--- scripts/scroller.js
+++ scripts/scroller.js
@@ -6,7 +6,7 @@ $(function() {
       if (target.length) {
         $('html,body').animate({
           scrollTop: target.offset().top
-        }, 1500);
+        }, 500);
         return false;
       }
     }
